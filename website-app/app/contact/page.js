9b4cd5771531a3440ff0adf4304f7b2ad56aeabe
JaySmyth21 <105235599+JaySmyth21@users.noreply.github.com>
2024-01-14 22:35:55 -0800

    added home, about, contact pages. added footer

---
+++ website-app/app/contact/page.js
@@ -0,0 +1,14 @@
+// ./pages/contact.js
+
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the Contact page of our website.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
